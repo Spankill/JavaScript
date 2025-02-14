3f45439698e6258eec4f96bf167e04bcc6a95b71
ARUHIZA CARLOS <aruhiza87@gmail.com>
2025-02-14 10:59:12 -0400

    nuevo endpoint actualizar users

--- mi-primer-backend/index.js
+++ mi-primer-backend/index.js
@@ -1,27 +1,28 @@
 const express = require('express');
 const app = express();
 const port = 3001;
+app.use(express.json());
 
 //ruta principal 
 app.get('/', (req, res) => {
-    res.send('HOLA MUNDO!');
+    return res.send('HOLA MUNDO!');
 });
 
 //endpoint para obtener datos de usuarios
 
 app.get('/api/datos', (req, res) => {
     const datos = {
-        nombre: 'Andres',
-        edad: 40,
+        name: 'Andres',
+        age: 40,
         ciudad: 'La Paz'
     };
-    res.json(datos);
+    return res.json(datos);
 })
 
 //Endpoint con parametros sencillos
-app.get('/api/saludo/:nombre', (req, res) => {
-    const nombre = req.params.nombre;
-    res.send('Hola, ' + nombre);
+app.get('/api/saludo/:name', (req, res) => {
+    const name = req.params.name;
+    return res.send('Hola, ' + name);
 });
 
 //endpoint que suma dos numeros
@@ -29,7 +30,7 @@ app.get('/api/suma/:num1/:num2', (req, res) => {
     const num1 = parseFloat(req.params.num1);
     const num2 = parseFloat(req.params.num2);
     const resultado = num1 + num2;
-    res.json(
+    return res.json(
         resultado);
 });
 
@@ -38,14 +39,14 @@ app.get('/api/resta/:num1/:num2', (req, res) => {
     const num1 = parseFloat(req.params.num1)
     const num2 = parseFloat(req.params.num2)
     const resultado = num1 - num2
-    res.json(resultado)
+    return res.json(resultado)
 });
 
 app.get('/api/multi/:num1/:num2', (req, res) => {
     const num1 = parseFloat(req.params.num1)
     const num2 = parseFloat(req.params.num2)
     const resultado = num1 * num2
-    res.json(resultado)
+    return res.json(resultado)
 });
 
 
@@ -53,11 +54,11 @@ app.get('/api/div/:num1/:num2', (req, res) => {
     const num1 = parseFloat(req.params.num1)
     const num2 = parseFloat(req.params.num2)
     if (num2 === 0) {
-        res.status(400).send('Error: Division por cero')
+        return res.status(400).send('Error: Division por cero')
     }
     else {
         const resultado = num1 / num2
-        res.json(resultado)
+        return res.json(resultado)
     }
 })
 
@@ -88,8 +89,52 @@ app.get('/api/user/:id', (req, res) => {
     else {
         res.status(404).json('Usuario no encontrado')
     }
-    res.json(id)
+    //return res.json(id) Por ser doble respuesta NUNCA COMETER ESTE ERROR!!!
 })
+
+//Endpoint para nuevo usuario creado
+app.post('/api/user', (req, res) => {
+    const newUser = {
+        id: users.length + 1,
+        name: req.body.name,
+        age: req.body.age
+    };
+    users.push(newUser)
+    res.status(201).json(newUser)
+});
+
+//Endpopont para actualizar un usuario
+app.put('/api/user/:id', (req, res) => {
+    const id = parseInt(req.params.id)
+    const userIndex = users.findIndex(u => u.id === id)
+    if (userIndex != -1) {
+        users[userIndex] = {
+            id,
+            name: req.body.name,
+            age: req.body.age
+        };
+        return res.json(users[userIndex])
+    }
+    else {
+        return res.status(404).json('Usuario no encontrado')
+    }
+});
+
+
+//Endpoint Elimina usuario
+app.delete('/api/user/:id', (req, res) => {
+    const id = parseInt(req.params.id)
+    const userIndex = users.findIndex(u => u.id === id)
+    if (userIndex !== -1) {
+        const userDelete = users.splice(userIndex, 1)
+        return res.json(userDelete)
+    }
+    else {
+        return res.status(404).json('Usuario no encontrado')
+    }
+})
+
+
 
 // inicio del servidor
 
