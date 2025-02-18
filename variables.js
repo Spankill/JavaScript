556f2f0e8f9542b6b56e84864c9ae2552cb5239d
ARUHIZA CARLOS <aruhiza87@gmail.com>
2025-02-18 01:02:22 -0400

    Tipos de variables JS

---
+++ variables.js
@@ -0,0 +1,20 @@
+// VAR
+
+var holaMundo = "Hello JS !"
+console.log(holaMundo);
+
+holaMundo = "Hello again JavaScript!"
+console.log(holaMundo);
+
+//LET
+
+let holaMundo2 = "Hello JS 2!"
+console.log(holaMundo2);
+
+holaMundo2 = "Hello again JS 2!"
+console.log(holaMundo2);
+
+// CONST
+const holaMundo3 = "Hello JS 3!"
+console.log(holaMundo3);
+
