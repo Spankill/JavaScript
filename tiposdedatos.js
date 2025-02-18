b6abc913bb3fe686d449921accf368e37843e5b5
ARUHIZA CARLOS <aruhiza87@gmail.com>
2025-02-18 12:13:32 -0400

    tipos de datos JS

---
+++ tiposdedatos.js
@@ -0,0 +1,36 @@
+let myName = "Carlos Aruhiza"
+let alias = 'Carlitos'
+let email = 'aruhiza87@gmail.com'
+
+let age = 99
+let height = 1.71
+
+let isStudent = true
+
+let undefinedValue
+console.log(undefinedValue)
+
+let nulValue = null
+
+let symbol = Symbol("mysymbol")
+
+let myBigInt = BigInt(765432345678765432)
+let myBigInt2 = 8765134876543
+
+console.log(typeof myName)
+console.log(typeof alias)
+console.log(typeof email)
+
+console.log(typeof age)
+console.log(typeof height)
+
+console.log(typeof isStudent)
+
+console.log(typeof undefinedValue)
+
+console.log(typeof nulValue)
+
+console.log(typeof symbol)
+
+console.log(typeof myBigInt)
+console.log(typeof myBigInt2)
