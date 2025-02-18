49207223aac664db8134ddb448c7b2f17f82d1c3
ARUHIZA CARLOS <aruhiza87@gmail.com>
2025-02-18 00:58:07 -0400

    JavaScript bases

---
+++ helloWorld.js
@@ -0,0 +1,21 @@
+// comentario
+
+/* 
+comentario 
+de 
+varias
+lineas
+
+*/
+
+console.log("HelloJS!")
+console.log("MIAU JS!")
+
+console.log("numero10")
+console.log(10)
+console.log(10 + 5)
+console.log(10 - 5);
+console.log(10 * 5);
+console.log(10 / 2);
+console.log(10 % 2);
+console.log(10 ** 2);
